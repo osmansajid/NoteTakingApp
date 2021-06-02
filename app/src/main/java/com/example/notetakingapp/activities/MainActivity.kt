23a108ef6769ab3a9e0bdf207b5418e47d9f9e46
Sajid <osmansazid13@gmail.com>
2021-06-02 16:31:46 +0600

    Integrated ViewModel to MainActivity and Updated gradle file

--- app/src/main/java/com/example/notetakingapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/notetakingapp/activities/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.notetakingapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.notetakingapp.R
+import com.example.notetakingapp.viewmodels.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
+    var noteViewModel: NoteViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProvider(this)[NoteViewModel::class.java]
+        noteViewModel!!.getAllNote().observe(this){
+            Toast.makeText(this,"onChanged",Toast.LENGTH_SHORT).show()
+        }
     }
 }
